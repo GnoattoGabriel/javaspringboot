a8aa81f154b5d736aece637b6a8365070d7cb1b3
unknown <gnoattogabriel100@gmail.com>
2026-02-24 21:50:29 -0300

    repositorio criado

---
+++ school/src/main/java/com/gnoatto/school/repoositories/AlunoRepository.java
@@ -0,0 +1,12 @@
+package com.gnoatto.school.repoositories;
+
+
+import com.gnoatto.school.models.AlunoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository  extends JpaRepository<AlunoModel, Long> {
+
+
+
+
+}
